cd0d9fbbe5c468798da3ec536a8dfc4ab8877ee3
immms <csi9926@gmail.com>
2024-01-16 20:57:54 +0900

    fix: 통신 코드 수정

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import serverConfig from '../../config';
 import '../../styles/intro.css';
 
@@ -13,17 +13,9 @@ const Signup = () => {
     memberPhone: ''
   });
 
+  const uri = 'api/signup';
 
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [id]: value }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const serverUrl = serverConfig.serverUrl;
-    const uri = '/api/signup';
+  const postSignup = () => {
 
     fetch(uri, {
       method: 'POST',
@@ -34,13 +26,13 @@ const Signup = () => {
     })
       .then((response) => {
         if (!response.ok) {
-          console.log('응답:', response);
-          throw new Error('네트워크 응답이 실패했다고.');
+          console.log('서버응답:', response);
+          throw new Error(`${response.status} ${response.statusText}`);
         }
         return response.json();
       })
       .then((responseData) => {
-        const resultMessage = '회원가입 성공: ' + responseData.message;
+        const resultMessage = responseData.message;
         alert(resultMessage);
         window.location.href = '/signin';
       })
@@ -48,104 +40,122 @@ const Signup = () => {
         const errorMessage = '오류 발생: ' + error.message;
         alert(errorMessage);
       });
+  }
+  // useEffect(() => {
+  //   postSignup();
+  // }, []);
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postSignup();
   };
 
   return (
-    <div className="signup-container">
-      <h1>회원가입</h1>
-      <form onSubmit={handleSubmit}>
+    <div className='intro'>
 
+      <div className="signup-container">
+        <h1>회원가입</h1>
+        
+        <form onSubmit={handleSubmit}>
 
-        {/* input fields */}
-        <div className="input-container">
-          <label htmlFor="memberName"> 이름</label>
-          <input
-            type="text"
-            id="memberName"
-            name="memberName"
-            value={formData.memberName}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-container">
+            <label htmlFor="memberName"> 이름</label>
+            <input
+              type="text"
+              id="memberName"
+              name="memberName"
+              value={formData.memberName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-container">
-          <label htmlFor="memberEmail">이메일</label>
-          <input
-            type="text"
-            id="memberEmail"
-            name="memberEmail"
-            value={formData.memberEmail}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-container">
+            <label htmlFor="memberEmail">이메일</label>
+            <input
+              type="text"
+              id="memberEmail"
+              name="memberEmail"
+              value={formData.memberEmail}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-container">
-          <label htmlFor="memberPw"> 비밀번호</label>
-          <input
-            type="password"
-            id="memberPw"
-            name="memberPw"
-            value={formData.memberPw}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-container">
+            <label htmlFor="memberPw"> 비밀번호</label>
+            <input
+              type="password"
+              id="memberPw"
+              name="memberPw"
+              value={formData.memberPw}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-container">
-          <label htmlFor="memberBaekId"> 백준 아이디</label>
-          <input
-            type="text"
-            id="memberBaekId"
-            name="memberBaekId"
-            value={formData.memberBaekId}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-container">
+            <label htmlFor="memberBaekId"> 백준 아이디</label>
+            <input
+              type="text"
+              id="memberBaekId"
+              name="memberBaekId"
+              value={formData.memberBaekId}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-container">
-          <label htmlFor="memberGen"> 기수</label>
-          <input
-            type="text"
-            id="memberGen"
-            name="memberGen"
-            value={formData.memberGen}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-container">
+            <label htmlFor="memberGen"> 기수</label>
+            <input
+              type="text"
+              id="memberGen"
+              name="memberGen"
+              value={formData.memberGen}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-container">
-          <label htmlFor="memberMajor"> 학과</label>
-          <input
-            type="text"
-            id="memberMajor"
-            name="memberMajor"
-            value={formData.memberMajor}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-container">
+            <label htmlFor="memberMajor"> 학과</label>
+            <input
+              type="text"
+              id="memberMajor"
+              name="memberMajor"
+              value={formData.memberMajor}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-container">
-          <label htmlFor="memberPhone"> 전화번호</label>
-          <input
-            type="text"
-            id="memberPhone"
-            name="memberPhone"
-            value={formData.memberPhone}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-container">
+            <label htmlFor="memberPhone"> 전화번호</label>
+            <input
+              type="text"
+              id="memberPhone"
+              name="memberPhone"
+              value={formData.memberPhone}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <button type="submit" className="btn_intro">
-          회원가입
-        </button>
-      </form>
+          <button type="submit" className="btn_intro">
+            회원가입
+          </button>
+        </form>
+      </div>
     </div>
+
   );
 };
 
