454621ac733cb255e964bb67b0c41779a6dc743e
byeon22 <minjeongs0202@gmail.com>
2024-02-09 03:57:01 +0900

    fix: answerId 유무에 따라 navigate url 변경

--- src/components/list/ListItem.js
+++ src/components/list/ListItem.js
@@ -4,11 +4,20 @@ import { StyledListItem } from "styles/ListItem-styled";
 const ProblemItem = (props) => {
     const navigate = useNavigate();
 
+    function goTo(answerId) {
+        if (answerId) {
+            navigate(`/question/${props.answerId}`);
+        }
+        else {
+            navigate(`/problem/${props.pid}`);
+        }
+    }
+
     return (
         <StyledListItem>
             <div className="container" state={props.state}>
-            <span className="problem_id" state={props.state} onClick={() => { navigate(`/question/${props.pid}`); }}>{props.pid}</span>
-            <span className="problem_title" onClick={() => { navigate(`/question/${props.pid}`); }}>{props.ptitle}</span>
+            <span className="problem_id" state={props.state} onClick={() => { goTo(props.answerId); }}>{props.pid}</span>
+            <span className="problem_title" onClick={() => { goTo(props.answerId); }}>{props.ptitle}</span>
             <span className="grade">점수 {props.grade}</span>
             <button className="button" state={props.state} onClick={() => navigate(`/solution/${props.pid}`)}>{props.state === "retry" ? "RE TRY" : "풀이보기"}</button>
             </div>
