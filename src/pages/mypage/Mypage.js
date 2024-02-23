5aa6893fa823b822f594894cb2af55c28c95b1ab
immms <csi9926@gmail.com>
2024-02-23 18:38:22 +0900

    feat: 마이페이지 내가푼 문제, 내 feedback 기능 추가

--- src/pages/mypage/Mypage.js
+++ src/pages/mypage/Mypage.js
@@ -5,6 +5,7 @@ import Footer from "components/footer/Footer";
 import '../../App.css';
 
 const MyPage = () => {
+    //정보리스트
     const [memberData, setMemberData] = useState({
       memberEmail: '',
       memberName: '',
@@ -14,11 +15,32 @@ const MyPage = () => {
       memberPhone: '',
       // memberScore: ''
     });
+
+    //내문제리스트
+    const [myProb, setMyProb] = useState({
+      problemId: '',
+      problemTitle: '',
+      problemScore: '',
+      answerId: '',
+      answerState: '',
+      finalScore: ''
+    });
   
+    //내피드백리스트
+    const [myFeedback, setMyFeedback] = useState({
+      answerId: '',
+      memberGen: '',
+      mameberName: '',
+      commentPassFail: '',
+      commentContent: ''
+    });
+
+
     // const serverUrl = serverConfig.serverUrl;
-    const uri = 'api/mypage/information';
   
-    const sendGetRequest = () => {
+    // 내 정보
+    const sendGetInfo = () => {
+      const uri = 'api/mypage/information';
   
       fetch(`${uri}`, {
         method: 'GET',
@@ -38,10 +60,59 @@ const MyPage = () => {
         });
     };
   
+    // 내가 푼 문제
+    const sendGetProb = () => {
+      const uri = 'api/mypage/mysolve';
+  
+      fetch(`${uri}`, {
+        method: 'GET',
+      })
+        .then(response => {
+          if (!response.ok) {
+            console.log('서버응답:', response);
+            throw new Error(`데이터 가져오기 실패: ${response.status} ${response.statusText}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          setMyProb(data);
+        })
+        .catch(error => {
+          console.error('데이터 가져오기 실패:', error);
+        });
+    };
+
+
+    // 내 feedback
+    const sendGetFeedback = () => {
+      const uri = 'api/mypage/myfeedback';
+  
+      fetch(`${uri}`, {
+        method: 'GET',
+      })
+        .then(response => {
+          if (!response.ok) {
+            console.log('서버응답:', response);
+            throw new Error(`데이터 가져오기 실패: ${response.status} ${response.statusText}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          setMyFeedback(data);
+        })
+        .catch(error => {
+          console.error('데이터 가져오기 실패:', error);
+        });
+    };
+
+
     useEffect(() => {
-      sendGetRequest();
+      sendGetInfo();
+      sendGetProb();
+      sendGetFeedback();
     }, []);
   
+
   
     return (
 
@@ -69,14 +140,30 @@ const MyPage = () => {
               {/* 내가 푼 문제 */}
               <div className='top'>
                 <div className='mypage_btn'>내가 푼 문제</div>
-                <div className='myProblem'></div>
+                
+                <div className='myProblem'>
+                  <div><p>{myProb.problemId}</p></div>
+                  <div><p>{myProb.problemTitle}</p></div>
+                  <div><p>{myProb.problemScore}</p></div>
+                  <div><p>{myProb.answerId}</p></div>
+                  <div><p>{myProb.answerState}</p></div>
+                  <div><p>{myProb.finalScore}</p></div>
+                </div>
               </div>
               
 
               {/* 내 피드백 */}
+              {/* 신입생,재학생에 따라 feedback 띄워주기 */}
               <div className='bottom'>
                 <div className='mypage_btn'>my feedback</div>
-                <div className='myFeedback'></div>
+                <div className='myFeedback'>
+                  <div><p>{myFeedback.answerId}</p></div>
+                  <div><p>{myFeedback.memberGen}</p></div>
+                  <div><p>{myFeedback.mameberName}</p></div>
+                  <div><p>{myFeedback.commentPassFail}</p></div>
+                  <div><p>{myFeedback.commentContent}</p></div>
+                  
+                </div>
               </div>
 
             </div>
