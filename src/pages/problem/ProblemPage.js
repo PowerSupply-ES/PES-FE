04321421f21b5ebc1945ea399a4030dd6bc9d5f4
byeon22 <minjeongs0202@gmail.com>
2024-02-23 19:46:15 +0900

    fix: solution 페이지 API 반복 요청 해결

--- src/pages/problem/ProblemPage.js
+++ src/pages/problem/ProblemPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import Header from "components/main/Header";
 import { StyledProblem } from 'styles/Problem-styled';
