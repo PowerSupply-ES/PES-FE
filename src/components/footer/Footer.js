c11f2db3538cdb34889e7c0b1800f00d465dd6f1
immms <csi9926@gmail.com>
2024-02-28 02:56:16 +0900

    design: footer제작

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,9 +12,27 @@ function Footer() {
 
 function Bottom(){ 
     return(
-       <div className='footer'>
+      <div className='footer'>
 
-       </div>
+        <div className='footer_left'>
+          <p className='logo'>PES</p>
+          <div className='info'>
+            <div className='icon_phone'> <p>010-8012-0865</p></div>
+            <div className='icon_address'> <p>경북 경산시 대학로 280 생산기술연구원 G15 213호</p></div>
+            <div className='icon_email'> <p>csi9926@gmail.com</p></div>
+            
+          </div>
+        </div>
+        <div className='footer_right'>
+            <p className='about'>About The Club</p>
+            <p className='info1'>영남대학교 로봇공학과 동아리 PowerSupply의</p>
+            <p className='info2'>교육시스템 개발팀 PES</p>
+            <div className='icon_sns'></div>
+        
+            <div className='footer_logout'>로그아웃</div>
+        </div>
+
+      </div>
     )
 }
 
