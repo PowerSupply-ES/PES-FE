58d9481203ea02f4f6d2f605184f1ce278ab6afb
immms <csi9926@gmail.com>
2024-02-28 03:14:10 +0900

    design: footer 아이콘 삽입

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { AiFillPhone} from "react-icons/ai";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { BiLogoInstagramAlt } from "react-icons/bi";
 
 function Footer() {
 
@@ -17,9 +21,9 @@ function Bottom(){
         <div className='footer_left'>
           <p className='logo'>PES</p>
           <div className='info'>
-            <div className='icon_phone'> <p>010-8012-0865</p></div>
-            <div className='icon_address'> <p>경북 경산시 대학로 280 생산기술연구원 G15 213호</p></div>
-            <div className='icon_email'> <p>csi9926@gmail.com</p></div>
+            <div className='phone'><AiFillPhone></AiFillPhone> <p>010-8012-0865</p></div>
+            <div className='address'><FaMapMarkerAlt></FaMapMarkerAlt> <p>경북 경산시 대학로 280 생산기술연구원 G15 213호</p></div>
+            <div className='email'><IoIosMail></IoIosMail> <p>csi9926@gmail.com</p></div>
             
           </div>
         </div>
@@ -27,7 +31,9 @@ function Bottom(){
             <p className='about'>About The Club</p>
             <p className='info1'>영남대학교 로봇공학과 동아리 PowerSupply의</p>
             <p className='info2'>교육시스템 개발팀 PES</p>
-            <div className='icon_sns'></div>
+            <div className='sns'>
+              <BiLogoInstagramAlt size={32}></BiLogoInstagramAlt >
+            </div>
         
             <div className='footer_logout'>로그아웃</div>
         </div>
