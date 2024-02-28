7e99419cdeffb5150a8e86f873b7f8cac7591eb6
byeon22 <minjeongs0202@gmail.com>
2024-02-28 16:33:39 +0900

    style: comment 입력창 및 결과창 스타일 적용

--- src/styles/Question-styled.js
+++ src/styles/Question-styled.js
@@ -103,28 +103,121 @@ export const StyledQuestion = styled.div`
         cursor: pointer;
         box-shadow: 1px 3px #CCC9C9;
     }
-    .title {
-        width: 1376px;
-    display: flex;
-    font-size: 28px;
-    font-weight: bold;
-    margin: 0 auto;
-    margin-top: 20px;
-    margin-bottom: 10px;
+    /* feedback (comment) */
+    .feedback_section {
+        width: 80%;
+        justify-content: space-between;
+        display: flex;
+        left: 50%;
+        transform: translateX(-50%);
+       position: relative;
     }
     .feedback_container {
-        width: 1376px;
-    height: auto;
-    display: flex;
-    align-items: center;
-    margin: auto;
-    border-radius: 5px;
-    background-color: beige;
-    font-size: 20px;
-    white-space: pre-line; 
-    margin-bottom: 10px;
+        width: 45%;
     }
-    
+    .feedback_result {
+        width: 100px;
+        text-align: left;
+        font-size: 50px;
+        font-weight: bold;
+
+        #pass {
+            color: #04ca00;
+        }
+
+        #fail {
+            color: rgba(244, 117, 117, 0.9);
+        }
+    }
+    .feedback_index {
+        width: 200px;
+        height: 60px;
+        border-radius: 50px;
+        line-height: 60px;
+        text-align: center;
+        background-color: #9747FF;
+    }
+    .feedback_writer {
+        margin-left: 10px;
+        font-size: 35px;
+    }
+    .feedback_content {
+        width: 100%;
+        font-size: 20px;
+        background-color: #ffffff;
+        border: solid #ffffff;
+        border-radius : 20px;
+        box-shadow: 1px 3px #CCC9C9;
+        overflow-wrap: break-word;
+        
+        textarea {
+            height: 270px;
+        }
+
+        div {
+            height: 250px;
+            text-align: left;
+            padding: 10px;
+            box-sizing: border-box;
+        }
+    }
+    .feedback_content::-webkit-scrollbar {
+    width: 10px;  
+    }
+    .feedback_content::-webkit-scrollbar-thumb {
+    background: #c0c0c0; /* 스크롤바 색상 */
+    border-radius: 10px; /* 스크롤바 둥근 테두리 */
+    }
+    .feedback_content::-webkit-scrollbar-track {
+    background: #e7e6f696;  /*스크롤바 뒷 배경 색상*/
+    }
+    .feedback_select_section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .button_container {
+        width: fit-content;
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+    }
+    .select_button {
+        font-size: 50px;
+        font-weight: bold;
+        cursor: pointer;
+        #pass {
+            color: ${(props) => (props.isSelected ? "rgba(4, 202, 0, 1)" : "rgba(4, 202, 0, 0.6)")};
+            &:hover {
+                color: rgba(4, 202, 0, 1);
+            }
+        }
+        #fail {
+            color: ${(props) => (props.isSelected ? "rgba(244, 117, 117, 1)" : "rgba(244, 117, 117, 0.6)")};
+            &:hover {
+                color: rgba(244, 117, 117, 1);
+            }
+        }
+    }
+    .select_comment {
+        font-size: 24px;
+        color: #6F6F6F;
+    }
+    .feedback_button {
+        width: 100%;
+        height: 80px;
+        border-radius: 5px;
+        background-color: rgba(109,99,228,0.6);
+        color: #ffffff;
+        font-size: 30px;
+        font-weight: bold;
+        align-items: center;
+        cursor: pointer;
+    }
+    .feedback_button:hover {
+    background-color: rgba(109,99,228,1);
+    }
     .pass {
         width: 70%;
         height: fit-content;
@@ -143,7 +236,6 @@ export const StyledQuestion = styled.div`
 
         cursor: pointer;
     }
-
     .fail {
         width: 70%;
         height: fit-content;
@@ -153,7 +245,7 @@ export const StyledQuestion = styled.div`
         margin: 100px auto;
         padding: 12px 0;
 
-        background-color: #EB3434;
+        background-color: rgba(244, 117, 117, 0.9);
         color: #ffffff;
         border-radius: 15px;
         font-size: 20px;
