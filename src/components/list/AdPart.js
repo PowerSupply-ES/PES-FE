91ec5f180bacebc281b39f502a92f1e6ef0f0fce
immms <csi9926@gmail.com>
2024-03-03 17:43:02 +0900

    feat: 에드핏 삽입

--- src/components/list/AdPart.js
+++ src/components/list/AdPart.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import {useRef, useEffect} from "react";
+
 import '../../App.css';
 
 
@@ -8,14 +10,42 @@ const AdContainer = styled.div`
     background-color: #272727;
     justify-content:center;
     width:100%;
-    height: 50vh;
+    height: 90px;
+    padding:0px 0;
     margin: auto;
     margin-bottom: 48px;
 `
 
 const AdPart = () => {
+    //카카오 에드핏 광고
+    const scriptElement = useRef(null);
+    // script 태그를 동적으로 추가
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.setAttribute(
+        "src",
+        "https://t1.daumcdn.net/kas/static/ba.min.js"
+        );
+        script.setAttribute(
+        "charset",
+        "utf-8"
+        );
+    
+        script.setAttribute("async", "true");
+        scriptElement.current?.appendChild(script);
+    }, []);
+
     return (
         <AdContainer>
+            <div ref={scriptElement}>
+                <ins
+                    className="kakao_ad_area"
+                    style={{ display: "none" }}
+                    data-ad-unit="DAN-vGny0Q5scRDlw6pg"
+                    data-ad-width="728"
+                    data-ad-height="90"
+                    />
+                </div>
         </AdContainer>
     );
 };
