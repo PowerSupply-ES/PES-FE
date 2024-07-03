c3d6a193132fe824ab15352c2d6ac0583a53e5a5
immms <csi9926@gmail.com>
2024-07-03 16:52:17 +0900

    feat: 회원목록api 적용

--- src/components/manage/UserList.js
+++ src/components/manage/UserList.js
@@ -1,31 +1,56 @@
 import { useNavigate } from 'react-router-dom';
 import '../../styles/css/manage.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 function UserList(){
     
     const navigate = useNavigate();
-    const [memDate, setMemDate] = useState();
+    const [memDate, setMemDate] = useState([]);
 
+    // 회원목록 api호출
+    const getUserList= async() =>{
+        const uri = `../api/admin/memberlist`
 
+        try{
+            const res = await axios.get(uri);
+            const memList = res.data;
+            setMemDate([...memList]);
+            console.log("memList = ",memList);
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    useEffect(()=>{
+        getUserList();
+    },[])
+
+    // 페이지 이동
     const gotoDetail = () =>{
         sessionStorage.setItem('memId', memDate.memberId);
         navigate(`/manageUser/${memDate.memberId}`);
-        
     }
 
-
     return(
-        <div className="list_wrapper">
-            <div className="title_state">재학생</div>
-            <div className="title_name">최성임</div>
-            <div className="title_stunum">21910867</div>
-            <div className="title_studep">생명과학과</div>
-            <div className="title_none">
-                <button className="levelup_btn">등업</button>
-                <button className="info_btn" onClick={()=>{}}>정보</button>
-            </div>      
-        </div>
+        <>
+        {memDate.map((mem) =>{
+            return(
+                <ul className="list_wrapper">
+        
+                <li className="title_state">{mem.memberStatus}</li>
+                <li className="title_name">{mem.memberName}</li>
+                <li className="title_stunum">{mem.memberId}</li>   {/* 회원id */}
+                <li className="title_studep">{mem.memberMajor}</li>
+                <li className="title_none">
+                    <button className="levelup_btn">등업</button>
+                    <button className="info_btn" onClick={()=>{}}>정보</button>
+                </li>      
+            </ul>
+            )
+        })}
+        </>
+        
     )
 }
 
