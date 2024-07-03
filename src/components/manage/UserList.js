c8c9f51cca2b291f6958a4ad7a7e8a3a7ac88cdd
immms <csi9926@gmail.com>
2024-07-04 01:13:13 +0900

    test: delete 버튼 색상오류

--- src/components/manage/UserList.js
+++ src/components/manage/UserList.js
@@ -12,17 +12,17 @@ function UserList({memberStatus, memberName, memberId, memberMajor}){
 
     return(
         <>
-            <ul className="list_wrapper">
-                <li className="title_state">{memberStatus}</li>
-                <li className="title_name">{memberName}</li>
-                <li className="title_stunum">{memberId}</li>   {/* 회원id */}
-                <li className="title_studep">{memberMajor}</li>
-                <li className="title_none">
+            <div className="list_wrapper">
+                <div className="title_state">{memberStatus}</div>
+                <div className="title_name">{memberName}</div>
+                <div className="title_stunum">{memberId}</div>   {/* 회원id */}
+                <div className="title_studep">{memberMajor}</div>
+                <div className="title_none">
                     <button className="levelup_btn">등업</button>
                     <button className="info_btn" onClick={()=>gotoDetail}>정보</button>
                     <button className="del_btn">삭제</button>
-                </li>      
-            </ul>
+                </div>      
+            </div>
         </>
     )
 }
