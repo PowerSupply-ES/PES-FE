b2f4fb1234b90a4c529d93c419817d7fc6214280
immms <csi9926@gmail.com>
2024-08-23 19:02:49 +0900

    rename: footer 함수 분리 및 폴더 이동

---
+++ src/layouts/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Bottom from "components/footer/Bottom";
+
+function Footer() {
+  return (
+    <div>
+      <Bottom />
+    </div>
+  );
+}
+
+export default Footer;
