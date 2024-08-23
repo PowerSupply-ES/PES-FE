e6fa43584e0977738954c1e86bc226f0df96f752
immms <csi9926@gmail.com>
2024-08-23 20:48:52 +0900

    refactor: MenuBar관련 코드 분리

--- src/components/main/MenuBar.js
+++ src/components/main/MenuBar.js
@@ -1,46 +1,14 @@
 import * as React from "react";
-import styled, { ThemeProvider } from "styled-components";
-import { createTheme, CssBaseline } from "@mui/material";
-import Box from "@mui/joy/Box";
+import { ThemeProvider } from "styled-components";
+import { CssBaseline } from "@mui/material";
 import Tabs from "@mui/joy/Tabs";
-import TabList from "@mui/joy/TabList";
-import Tab, { tabClasses } from "@mui/joy/Tab";
-import { useNavigate, useLocation} from "react-router-dom";
-import { useState, useEffect } from "react";
+import Tab from "@mui/joy/Tab";
+import MenuHook from "hooks/menu/MenuHook";
+import {StyledBox, theme, StyledTabList } from "styles/styledComponent/Menu-styled";
 
 function MenuBar() {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  // 현재 경로에 따라 초기 탭 인덱스 설정
-  const getInitialIndex = () => {
-    if (location.pathname === '/list') return 0;
-    if (location.pathname === '/notice') return 1;
-    return null; // 홈이나 기타 경로인 경우 탭이 선택되지 않도록
-  };
-
-  const [index, setIndex] = useState(getInitialIndex());
-
-  // 탭 이동
-  const toggleTab = (value) => {
-    setIndex(value);
-    if (value === 0) {
-      navigate('/list');
-    } else if (value === 1) {
-      navigate('/notice');
-    }
-  };
-
-  useEffect(() => {
-    // 경로가 '/'일 때 아무 탭도 선택되지 않도록 설정
-    if (location.pathname === '/' && index !== null) {
-      setIndex(null);
-    } else if (location.pathname === '/list' && index !== 0) {
-      setIndex(0);
-    } else if (location.pathname === '/notice' && index !== 1) {
-      setIndex(1);
-    }
-  }, [location.pathname, index]);
+  // 훅 사용
+  const {index, toggleTab} = MenuHook();
 
   return (
     <ThemeProvider theme={theme}>
@@ -61,39 +29,3 @@ function MenuBar() {
   );
 }
 export default MenuBar;
-
-const theme = createTheme();
-
-const StyledBox = styled(Box)`
-  flex-grow: 1;
-  margin: -2px;
-  overflow-x: hidden;
-  overflow-y: hidden;
-`;
-
-const StyledTabList = styled(TabList)`
-  padding-top: 8px;
-  justify-content: left;
-
-  && .${tabClasses.root} {
-    flex: initial;
-    background-color: transparent;
-
-    &:hover {
-      background-color: transparent;
-    }
-
-    &.${tabClasses.selected} {
-      color: ${(props) => props.theme.palette.primary.main};
-
-      &::after {
-        height: 2.5px;
-        border-top-left-radius: 3px;
-        border-top-right-radius: 3px;
-        background-color: ${(props) => props.theme.palette.primary[500]};
-      }
-    }
-
-    font-size: 26px;
-  }
-`;
