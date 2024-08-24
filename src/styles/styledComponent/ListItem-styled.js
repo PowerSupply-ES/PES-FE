325f501181bb0362c504b3ae77bf6ffe0e2727cc
immms <csi9926@gmail.com>
2024-08-24 21:23:37 +0900

    chore: 스타일 파일 prettier 적용

--- src/styles/styledComponent/ListItem-styled.js
+++ src/styles/styledComponent/ListItem-styled.js
@@ -1,87 +1,111 @@
 import styled from "styled-components";
 
 export const StyledListItem = styled.div`
-    .container {
-        display: flex;
-        flex-direction: row;
-        height: fit-content;
-        align-items: center;
-        margin: auto;
-        padding: 8px 16px 8px 0px;
-        border-radius: 8px;
+  .container {
+    display: flex;
+    flex-direction: row;
+    height: fit-content;
+    align-items: center;
+    margin: auto;
+    padding: 8px 16px 8px 0px;
+    border-radius: 8px;
 
-        background-color: ${props => props.state === "locked" ? "#DEDEDE"
-            : props.state === "comment" ? "rgba(243, 242, 242, 0.737)"
-            : props.state === "question" ? "rgba(243, 242, 242, 0.737)"
-            
-            : "white"};
-        transition: background-color 0.3s; /* Add transition for smooth color change */
-        &:hover {
-            background-color: ${props => props.state === "locked" ? "#DEDEDE" 
-            : "rgba(243, 242, 242, 0.737)"};
-        }
+    background-color: ${(props) =>
+      props.state === "locked"
+        ? "#DEDEDE"
+        : props.state === "comment"
+        ? "rgba(243, 242, 242, 0.737)"
+        : props.state === "question"
+        ? "rgba(243, 242, 242, 0.737)"
+        : "white"};
+    transition: background-color 0.3s; /* Add transition for smooth color change */
+    &:hover {
+      background-color: ${(props) =>
+        props.state === "locked" ? "#DEDEDE" : "rgba(243, 242, 242, 0.737)"};
     }
+  }
 
-    .problem_id {
-        width: 100px;
-        height: 34px;
-        color: ${props => props.state === "success" ? "#04CA00" : props.state === "fail" ? "#FF8412" : "black"};
-        font-size: 18px;
-        text-align: center;
-        line-height: 34px;
-        
+  .problem_id {
+    width: 100px;
+    height: 34px;
+    color: ${(props) =>
+      props.state === "success"
+        ? "#04CA00"
+        : props.state === "fail"
+        ? "#FF8412"
+        : "black"};
+    font-size: 18px;
+    text-align: center;
+    line-height: 34px;
+  }
+
+  .problem_title {
+    flex: 1; /* Take up remaining space */
+    font-size: 20px;
+    text-align: left;
+    font-weight: bold;
+
+    &:hover {
+      cursor: pointer;
     }
+    color: #000000;
+    opacity: 70%;
+  }
 
-    .problem_title {
-        flex: 1; /* Take up remaining space */
-        font-size: 20px;
-        text-align: left; 
-        font-weight: bold;
+  .grade {
+    height: 34px;
+    font-size: 16px;
+    margin-right: 20px;
+    line-height: 34px;
+    font-weight: bold;
+    color: #000000;
+    opacity: 60%;
+  }
 
-        &:hover {
-            cursor: pointer;
-        }
-        color: #000000; opacity:70%;
-        
+  .button {
+    width: fit-content;
+    height: fit-content;
+    padding: 7px 30px;
+    box-shadow: 1px 1px #ccc9c9;
+
+    color: ${(props) =>
+      props.state === "success" || props.state === "fail"
+        ? "white"
+        : "#56565692"};
+
+    background-color: ${(props) =>
+      props.state === "success"
+        ? "rgba(109, 99, 228, 0.8)"
+        : props.state === "fail"
+        ? "rgba(244, 117, 117, 0.8)"
+        : // 문제를 푼상태는 버튼 흰색(comment이나 question)
+        props.state === "comment" || "question"
+        ? "white"
+        : // : props.state === null ? "#d6d6d6cc"
+          // 그외는 회색
+          "#d6d6d6cc"};
+    &:hover {
+      background-color: ${(props) =>
+        props.state === "success"
+          ? "rgba(109, 99, 228, 1.0)"
+          : props.state === "fail"
+          ? "rgba(244, 117, 117, 1.0)"
+          : ""};
     }
+    border-radius: 8px;
+    font-size: 17px;
+    font-weight: bold;
 
-    .grade {
-        height: 34px;
-        font-size: 16px;
-        margin-right: 20px;
-        line-height: 34px;
-        font-weight: bold;
-        color: #000000; opacity:60%;
-        }
-
-    .button {
-        width: fit-content;
-        height: fit-content;
-        padding: 7px 30px;
-        box-shadow: 1px 1px #CCC9C9;
-        
-        color: ${props => (props.state === "success" || props.state === "fail") ? "white" : "#56565692"};
-        
-        background-color: ${props => props.state === "success" ? "rgba(109, 99, 228, 0.8)" 
-            : props.state === "fail" ? "rgba(244, 117, 117, 0.8)"
-            // 문제를 푼상태는 버튼 흰색(comment이나 question)
-            : props.state === "comment" || "question" ? "white"
-            // : props.state === null ? "#d6d6d6cc"
-            // 그외는 회색
-            : "#d6d6d6cc"};
-        &:hover {
-            background-color: ${props => props.state === "success" ? "rgba(109, 99, 228, 1.0)" : props.state === "fail" ? "rgba(244, 117, 117, 1.0)" : ""};
-          }
-        border-radius: 8px;
-        font-size: 17px;
-        font-weight: bold;
-
-        text-align: center;
-        border: ${props => (props.state === "success" || props.state === "fail") ? "none" 
-            : props.state === null ? "#d6d6d6cc"
-            : "2px solid #DEDEDE"};
-        transition: background-color 0.3s;
-        margin-right: 14px;
-        pointer-events: ${props => (props.state === "success" || props.state === "fail") ? "auto" : "none"};
-    }
+    text-align: center;
+    border: ${(props) =>
+      props.state === "success" || props.state === "fail"
+        ? "none"
+        : props.state === null
+        ? "#d6d6d6cc"
+        : "2px solid #DEDEDE"};
+    transition: background-color 0.3s;
+    margin-right: 14px;
+    pointer-events: ${(props) =>
+      props.state === "success" || props.state === "fail" ? "auto" : "none"};
+  }
 `;
--- src/styles/styledComponent/ProblemItem-styled.js
+++ src/styles/styledComponent/ProblemItem-styled.js
@@ -1,54 +1,68 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 1374px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    margin: auto;
-    border-radius: 5px;
-    background-color: ${props => props.state == "locked" ? "#DEDEDE" : "white"};
-    transition: background-color 0.3s; /* Add transition for smooth color change */
-    &:hover {
-        background-color: ${props => props.state == "locked" ? "#DEDEDE" : "#DEDEDE80"};
-    }
-`
+  width: 1374px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  margin: auto;
+  border-radius: 5px;
+  background-color: ${(props) =>
+    props.state == "locked" ? "#DEDEDE" : "white"};
+  transition: background-color 0.3s; /* Add transition for smooth color change */
+  &:hover {
+    background-color: ${(props) =>
+      props.state == "locked" ? "#DEDEDE" : "#DEDEDE80"};
+  }
+`;
 export const ProblemId = styled.span`
-    width: 100px;
-    height: 34px;
-    color: ${props => props.state === "solved" ? "#04CA00" : props.state === "retry" ? "#FF8412" : "black"};
-    font-size: 28px;
-    text-align: center;
-`
+  width: 100px;
+  height: 34px;
+  color: ${(props) =>
+    props.state === "solved"
+      ? "#04CA00"
+      : props.state === "retry"
+      ? "#FF8412"
+      : "black"};
+  font-size: 28px;
+  text-align: center;
+`;
 export const ProblemTitle = styled.span`
-    flex: 1;
-    font-size: 28px;
-    text-align: left; 
-    &:hover {
-        cursor: pointer;
-    }
-`
+  flex: 1;
+  font-size: 28px;
+  text-align: left;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 export const Grade = styled.span`
-    height: 34px;
-    font-size: 28px;
-    text-align: center;
-    margin-right: 20px;
-`
+  height: 34px;
+  font-size: 28px;
+  text-align: center;
+  margin-right: 20px;
+`;
 export const Button = styled.button`
-    width: 150px;
-    height: 42px;
-    color: ${props => props.state == "locked" ? "#6A6B6F" : "white"};
-    background-color: ${props => props.state === "baekjoon" ? "#5094f9" 
-        : props.state === "challenge" ? "#5094f9" 
-        : props.state === "solved" ? "#04CA00" 
-        : props.state === "retry" ? "#FF8412" 
-        : "white"};
-    border-radius: 5px;
-    font-size: 28px;
-    text-align: center;
-    border: ${props => props.state == "locked" ? "2px solid #DEDEDE" : "none"};
-    transition: background-color 0.3s;
-    /* &:hover {
-        background-color: ${props => props.color ? `${props.color}80` : "rgba(255, 255, 255, 0.8)"};
+  width: 150px;
+  height: 42px;
+  color: ${(props) => (props.state == "locked" ? "#6A6B6F" : "white")};
+  background-color: ${(props) =>
+    props.state === "baekjoon"
+      ? "#5094f9"
+      : props.state === "challenge"
+      ? "#5094f9"
+      : props.state === "solved"
+      ? "#04CA00"
+      : props.state === "retry"
+      ? "#FF8412"
+      : "white"};
+  border-radius: 5px;
+  font-size: 28px;
+  text-align: center;
+  border: ${(props) =>
+    props.state == "locked" ? "2px solid #DEDEDE" : "none"};
+  transition: background-color 0.3s;
+  /* &:hover {
+        background-color: ${(props) =>
+    props.color ? `${props.color}80` : "rgba(255, 255, 255, 0.8)"};
     } */
-`
+`;
