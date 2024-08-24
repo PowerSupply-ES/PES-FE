1d50e4a8581de81f41aaedf76ce2f0f3b5b957a0
immms <csi9926@gmail.com>
2024-08-24 20:58:46 +0900

    refactor: mypage관련 axios로 변경

--- src/apis/mypage/getMyInfo.js
+++ src/apis/mypage/getMyInfo.js
@@ -1,24 +1,24 @@
+import axios from 'axios';
+
 // 내 정보 GET 요청
-const getMyInfo = (setMemberData) => {
-    const uri = '/api/mypage/information';
-  
-    fetch(`${uri}`, {
-      method: 'GET',
-    })
-      .then(response => {
-        if (!response.ok) {
-          console.log('서버응답:', response);
-          throw new Error(`데이터 가져오기 실패: ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        setMemberData(data);
+const getMyInfo = async (setMemberData) => {
+  const uri = '/api/mypage/information';
 
-      })
-      .catch(error => {
-        console.error('데이터 가져오기 실패:', error);
-      });
-}
+  try {
+    const response = await axios.get(uri);
+    setMemberData(response.data);
+  } catch (error) {
+    if (error.response) {
+        // 서버가 응답을 반환했지만 상태 코드가 오류를 나타내는 경우
+        console.error('데이터 가져오기 실패:', error.response.status, error.response.statusText);
+    }else if (error.request) {
+        // 요청이 서버로 전송되었지만 응답을 받지 못한 경우
+        console.error('데이터 가져오기 실패: 요청이 실패했습니다.');
+    }else {
+        // 요청 설정 중 오류 발생
+        console.error('데이터 가져오기 실패:', error.message);
+    }
+  }
+};
 
-export default getMyInfo
+export default getMyInfo;
