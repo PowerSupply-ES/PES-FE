c8bc1550681ec6327e272dd0bae511ba16560db3
immms <csi9926@gmail.com>
2024-09-13 14:55:38 +0900

    feat: memberStatus 관리자일시 마이페이지에서 관리자모드 접근 가능

--- src/pages/mypage/mypage.tsx
+++ src/pages/mypage/mypage.tsx
@@ -2,16 +2,31 @@ import React from "react";
 import "styles/css/App.css";
 import "styles/css/mypage.css";
 import {MyFeedList, MyInfo, MyProbList} from "components/mypage";
+import { useNavigate } from "react-router-dom";
 
 // MyPage 컴포넌트 Props 타입
 interface MyPageProps {}
 
 const MyPage:React.FC<MyPageProps> = () => {
+  const navigate = useNavigate();
+  const memberStatus = sessionStorage.getItem('memberStatus');
+  console.log('memberStatus = ', memberStatus);
+
   return (
     <div className="content_mypage">
       <div className="mypage_content">
         {/* 왼쪽 내 정보 */}
-        <MyInfo />
+        <div className="left">
+          <MyInfo />
+          
+          {/* 관리자 등급이면 버튼표시 */}
+          {memberStatus==="관리자" ? (
+            <button className="btn_goto_manage" onClick={() => navigate('/manageUser')}>관리자모드</button>
+            ) : (
+            <></>
+            )
+          }
+        </div>
 
         <div className="right">
           {/* 내가 푼 문제 */}
