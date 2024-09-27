b207885db052adef109f71f1a26a825bcd1c0eca
immms <csi9926@gmail.com>
2024-09-27 20:27:11 +0900

    fix: 풀이열람 권한에 따른 예외처리 수정

--- src/pages/question/questionPage.tsx
+++ src/pages/question/questionPage.tsx
@@ -1,8 +1,7 @@
-import React, { ChangeEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import useQuestionHook from "hooks/question/useQuestionHook";
-import {RenderAnswerUI, RenderFeed} from "components/question";
-
+import { RenderAnswerUI, RenderFeed } from "components/question";
 
 const QuestionPage: React.FC = () => {
   const navigate = useNavigate();
@@ -10,7 +9,7 @@ const QuestionPage: React.FC = () => {
   // questionId 저장 - 사용자 풀이에 사용
   let url = new URL(window.location.href);
   let answerId = Number(url.pathname.split("/")[2]);
-  console.log("answerId= ",answerId);
+  console.log("answerId= ", answerId);
 
   // problemId 가져오기 - 해당 문제 정보에 사용
   const problemId = Number(sessionStorage.getItem("problemId"));
@@ -18,10 +17,9 @@ const QuestionPage: React.FC = () => {
   // 첫번재 답변 관리 - useRef로 필요 시점에만 상태 관리
   const [textFst, setTextFst] = useState<string>("");
 
-
   // 첫번재 답변 작성 핸들러
   const handleTextFstChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setTextFst(e.target.value)
+    setTextFst(e.target.value);
     console.log("e.target.value = ", e.target.value);
   };
 
@@ -47,12 +45,16 @@ const QuestionPage: React.FC = () => {
   } = useQuestionHook(answerId, getAlert, problemId);
 
   // useMemberStatus HOOK 호출 -> 수정: sessionStorage 사용
-  const memberStatus =sessionStorage.getItem("memberStatus");
+  const memberStatus = sessionStorage.getItem("memberStatus");
 
-  if (memberStatus === null || qnA === null) {
-    return <div className="loading">Loading...</div>;
-  }
+  useEffect(()=>{
+    if (memberStatus === null || qnA === null) {
+      alert("풀이 열람 권한이 없습니다!");
+      navigate(-1);
+    }
+  },[memberStatus, qnA, navigate]);
 
+  
   return (
     // 성공 or 실패에 따른 배경색
     <div
@@ -63,7 +65,7 @@ const QuestionPage: React.FC = () => {
           ? "failPage"
           : "nonePage"
       }
-    > 
+    >
       <div>
         {/* qnA가 null일 경우 처리 */}
         {qnA && (
@@ -99,19 +101,3 @@ const QuestionPage: React.FC = () => {
 };
 
 export default QuestionPage;
-
-// ==== answerState 구분 ====
-// - 질문 상태(question): 코드가 정상적으로 실행되어 질문에 답해야하는 상태 → 질문 테스트로 이동
-//     => qnA O
-// - 커멘트 상태(comment): 커멘트를 기다리는 상태 → 질문테스트 답변 결과로 이동
-//     => qnA O / qnA.answerFst, qnA.answerSec O / feedback 있을 수도 없을 수도
-// - 성공 상태(success): 모든 커멘트 sucess → 다른 사람의 코드도 확인 가능
-// - 실패 상태(fail): 실패 상태, 커멘트 중 fail있음→ 다른 사람의 코드 확인 가능
-
-// ==== TODO ====
-// 피드백 구분
-// 모든 커멘트 pass
-// fail 있는 경우
-// 신입생, 본인 글에는 댓글 달 수 없음 처리
-// 댓글 중복으로 달 수 없음 처리
-// 2명까지만 달 수 있음 처리
