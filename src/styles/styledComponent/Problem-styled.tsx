9a38f451b686e3c7109ff54cbf1c5a83f2d787ea
immms <csi9926@gmail.com>
2025-03-29 20:29:43 +0900

    fix: 코드에디터 공백오류 수정

--- src/styles/styledComponent/Problem-styled.tsx
+++ src/styles/styledComponent/Problem-styled.tsx
@@ -108,6 +108,11 @@ export const StyledProblem = styled.div<StyledProps>`
         justify-content: flex-start;
         margin: auto 0;
         height:100%;
+
+        padding: 0 !important;
+        margin: 0 !important;
+        letter-spacing: 0 !important; /* 글자 간격 초기화 */
+        line-height: normal !important; /* 줄 높이 초기화 */
     }
 
     .content_container {
@@ -119,11 +124,6 @@ export const StyledProblem = styled.div<StyledProps>`
         display: flex;
         flex-direction: column;
         margin-right: 60px;
-        // background-color: #ffffff;
-        // border-radius : 20px;
-        // box-shadow: 1px 3px #CCC9C9;
-        //   overflow-y: auto;
-
     }
     .content_container > .top,
     .content_container > .bottom{
@@ -221,7 +221,6 @@ export const StyledProblem = styled.div<StyledProps>`
         height: 100%;
         min-height: 564px;
         font-size: 18px;
-        // color: #ffffff;
         display: flex;
         justify-content: center;
         background-color: #3e3e3e;
@@ -276,13 +275,13 @@ export const StyledProblem = styled.div<StyledProps>`
     }
     .result_container {
         width: 1376px;
-    height: auto;
-    background-color: #eee;
-    margin: 0 auto;
-    margin-bottom: 20px;
-    font-size: 20px;
-    white-space: pre-line; 
-    text-align: left;
+        height: auto;
+        background-color: #eee;
+        margin: 0 auto;
+        margin-bottom: 20px;
+        font-size: 20px;
+        white-space: pre-line; 
+        text-align: left;
     }
     .loading {
         height: 1500px
